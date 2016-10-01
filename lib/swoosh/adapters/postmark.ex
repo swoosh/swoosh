a2e0e87207806b92c9614277ca8884dfe143e913
Kamil Lelonek <squixy.sln@gmail.com>
2016-10-01 12:56:35 +0200

    Reformat code

--- lib/swoosh/adapters/postmark.ex
+++ lib/swoosh/adapters/postmark.ex
@@ -26,8 +26,8 @@ defmodule Swoosh.Adapters.Postmark do
 
   def deliver(%Email{} = email, config \\ []) do
     headers = prepare_headers(config)
-    params = email |> prepare_body |> Poison.encode!
-    url = [base_url(config), @api_endpoint]
+    params  = email |> prepare_body() |> Poison.encode!()
+    url     = [base_url(config), @api_endpoint]
 
     case :hackney.post(url, headers, params, [:with_body]) do
       {:ok, 200, _headers, body} ->
@@ -42,10 +42,12 @@ defmodule Swoosh.Adapters.Postmark do
   defp base_url(config), do: config[:base_url] || @base_url
 
   defp prepare_headers(config) do
-    [{"User-Agent", "swoosh/#{Swoosh.version}"},
-     {"X-Postmark-Server-Token", config[:api_key]},
-     {"Content-Type", "application/json"},
-     {"Accept", "application/json"}]
+    [
+      {"User-Agent",              "swoosh/#{Swoosh.version}"},
+      {"X-Postmark-Server-Token", config[:api_key]},
+      {"Content-Type",            "application/json"},
+      {"Accept",                  "application/json"}
+    ]
   end
 
   defp prepare_body(email) do
@@ -67,10 +69,10 @@ defmodule Swoosh.Adapters.Postmark do
   defp prepare_cc(body, %Email{cc: []}), do: body
   defp prepare_cc(body, %Email{cc: cc}), do: Map.put(body, "Cc", prepare_recipients(cc))
 
-  defp prepare_bcc(body, %Email{bcc: []}), do: body
+  defp prepare_bcc(body, %Email{bcc: []}),  do: body
   defp prepare_bcc(body, %Email{bcc: bcc}), do: Map.put(body, "Bcc", prepare_recipients(bcc))
 
-  defp prepare_reply_to(body, %Email{reply_to: nil}), do: body
+  defp prepare_reply_to(body, %Email{reply_to: nil}),              do: body
   defp prepare_reply_to(body, %Email{reply_to: {_name, address}}), do: Map.put(body, "ReplyTo", address)
 
   defp prepare_recipients(recipients) do
@@ -79,14 +81,14 @@ defmodule Swoosh.Adapters.Postmark do
     |> Enum.join(",")
   end
 
-  defp prepare_recipient({"", address}), do: address
+  defp prepare_recipient({"",   address}), do: address
   defp prepare_recipient({name, address}), do: "\"#{name}\" <#{address}>"
 
   defp prepare_subject(body, %Email{subject: subject}), do: Map.put(body, "Subject", subject)
 
-  defp prepare_text(body, %{text_body: nil}), do: body
+  defp prepare_text(body, %{text_body: nil}),       do: body
   defp prepare_text(body, %{text_body: text_body}), do: Map.put(body, "TextBody", text_body)
 
-  defp prepare_html(body, %{html_body: nil}), do: body
+  defp prepare_html(body, %{html_body: nil}),       do: body
   defp prepare_html(body, %{html_body: html_body}), do: Map.put(body, "HtmlBody", html_body)
 end
