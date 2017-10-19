a4ce3d9a1cc296f445dc9b9ca0640c64f1797422
Anthony Smith <anthony@sticksnleaves.com>
2017-10-19 10:28:44 -0400

    Fixed tests

--- test/swoosh/template_test.exs
+++ test/swoosh/template_test.exs
@@ -2,12 +2,16 @@ defmodule Swoosh.TemplateTest do
   use ExUnit.Case, async: true
 
   defmodule FakeTemplate do
-    use Swoosh.Template, root: Path.join(__DIR__, "../fixtures/templates")
+    use Swoosh.Template, root: Path.join(__DIR__, "../support/fixtures/templates")
+
+    def render() do
+      render_template("template", %{ subject: "world" })
+    end
   end
 
   describe "render/2" do
     test "returns a compiled template" do
-      assert FakeTemplate.render("template", %{ subject: "world" }) == "Hello, world!\n"
+      assert FakeTemplate.render() == "Hello, world!\n"
     end
   end
 end
