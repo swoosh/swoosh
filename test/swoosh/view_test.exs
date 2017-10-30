3ec9ad3a3d73d8a62821c76be03f85168b6cc6e4
Anthony Smith <anthony@sticksnleaves.com>
2017-10-30 16:31:03 -0400

    Moved test views into fixtures directory

--- test/swoosh/view_test.exs
+++ test/swoosh/view_test.exs
@@ -1,27 +1,21 @@
+Code.require_file "../support/fixtures/views.exs", __DIR__
+
 defmodule Swoosh.ViewTest do
   use ExUnit.Case, async: true
 
-  defmodule FakeLayout do
-    use Swoosh.View, root: Path.join(__DIR__, "../support/fixtures/templates/layout")
-  end
-
-  defmodule FakeView do
-    use Swoosh.View, root: Path.join(__DIR__, "../support/fixtures/templates")
-  end
-
   describe "render/3" do
     test "renders template" do
-      assert Swoosh.View.render(FakeView, "template_1", %{}) ==
+      assert Swoosh.View.render(MyApp.FakeView, "template_1", %{}) ==
         "Hello, world!\n"
     end
 
     test "renders layout" do
-      assert Swoosh.View.render(FakeView, "template_1", layout: { FakeLayout, "layout" }) ==
+      assert Swoosh.View.render(MyApp.FakeView, "template_1", layout: { MyApp.FakeLayout, "layout" }) ==
         "This is a layout!\n\nHello, world!\n"
     end
 
     test "assigns variables" do
-      assert Swoosh.View.render(FakeView, "template_2", subject: "world") ==
+      assert Swoosh.View.render(MyApp.FakeView, "template_2", subject: "world") ==
         "Hello, world!\n"
     end
   end
