54e2ea819ef4da56155ee5ebc869971c026a955b
Theo Dowling <thdowling@gmail.com>
2018-10-24 18:02:38 +0200

    Store attachment content in Memory when using local adaptor

--- lib/swoosh/adapters/local/storage/memory.ex
+++ lib/swoosh/adapters/local/storage/memory.ex
@@ -119,6 +119,12 @@ defmodule Swoosh.Adapters.Local.Storage.Memory do
   def handle_call({:push, email}, _from, emails) do
     id = :crypto.strong_rand_bytes(16) |> Base.encode16() |> String.downcase()
     email = email |> Swoosh.Email.header("Message-ID", id)
+
+    attachments_with_data = email.attachments |> Enum.map(fn(attachment) -> 
+      %{attachment | data: Swoosh.Attachment.get_content(attachment)}
+    end)
+    
+    email = %{email | attachments: attachments_with_data}
     {:reply, email, [email | emails]}
   end
 
