226cd24bdd9388e1e7d6344597b722092b9b14d0
Benjamin Milde <benni@kobrakai.de>
2021-01-20 12:36:23 +0100

    Add the commas 😂

--- .formatter.exs
+++ .formatter.exs
@@ -3,9 +3,9 @@
   inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
   export: [
     locals_without_parens: [
-      assert_email_not_sent: 1
-      assert_email_sent: 0
-      assert_email_sent: 1
+      assert_email_not_sent: 1,
+      assert_email_sent: 0,
+      assert_email_sent: 1,
       assert_no_email_sent: 0,
       refute_email_sent: 0,
       refute_email_sent: 1
