23a4f06036a43d676b0c8e57a50ee04ab0dc7a8d
Aiden Scandella <git@sca.ndella.com>
2022-04-26 10:34:11 -0700

    Add ExAwsAmazonSES adapter

--- lib/swoosh/adapters/amazon_ses.ex
+++ lib/swoosh/adapters/amazon_ses.ex
@@ -63,6 +63,10 @@ defmodule Swoosh.Adapters.AmazonSES do
   include additional `X-Amz-Security-Token` header to that request.
 
   You can do that by adding `:security_token` to `:provider_options`.
+
+  If you don't have a static `:access_key` and `:secret_key` for your
+  application, you can use the `ExAwsAmazonSES` adapter to fetch credentials
+  on-demand as specified in your application's `:ex_aws` configuration.
   """
 
   use Swoosh.Adapter,
