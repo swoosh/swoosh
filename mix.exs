d1b5b4ff20ebb268f41857215c04bb49ec40d7b6
Po Chen <chenpaul914@gmail.com>
2023-06-22 09:41:03 +1000

    remove override

--- mix.exs
+++ mix.exs
@@ -73,7 +73,6 @@ defmodule Swoosh.Mixfile do
       {:cowboy, "~> 1.1 or ~> 2.4", optional: true},
       {:plug_cowboy, ">= 1.0.0", optional: true},
       {:ex_aws, "~> 2.1", optional: true},
-      {:ssl_verify_fun, ">= 0.0.0", only: :test, manager: :rebar3, override: true},
       {:bypass, "~> 2.1", only: :test},
       {:ex_doc, "~> 0.26", only: :docs, runtime: false}
     ]
