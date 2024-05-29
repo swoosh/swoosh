df0f9dcf5b3f27738bc1ebe1be44473ca363d8cd
Mickael Lucchini <hello@mickael.lu>
2024-05-29 15:26:39 -0500

    failing test when attempting to override region

--- test/swoosh/adapters/ex_aws_amazonses_test.exs
+++ test/swoosh/adapters/ex_aws_amazonses_test.exs
@@ -63,6 +63,37 @@ defmodule Swoosh.Adapters.ExAwsAmazonSESTest do
              {:ok, %{id: "messageId", request_id: "requestId"}}
   end
 
+  test "unsupported region can be overridden with a supported region", %{
+    bypass: bypass,
+    config: config,
+    valid_email: email
+  } do
+    Application.put_env(:ex_aws, :access_key_id, "FAKE")
+    Application.put_env(:ex_aws, :secret_access_key, "FAKE")
+    Application.put_env(:ex_aws, :region, "ap-east-1")
+
+    on_exit(fn ->
+      Application.put_env(:ex_aws, :access_key_id, nil)
+      Application.put_env(:ex_aws, :secret_access_key, nil)
+      Application.put_env(:ex_aws, :region, nil)
+    end)
+
+    Bypass.expect(bypass, fn conn ->
+      import Plug.Conn
+
+      [authorization_header] = get_req_header(conn, "authorization")
+      assert "AWS4-HMAC-SHA256 Credential=FAKE/" <> _ = authorization_header
+      assert authorization_header =~ "us-east-1"
+
+      resp(conn, 200, @success_response)
+    end)
+
+    config = Keyword.put(config, :region, "us-east-1")
+
+    assert ExAwsAmazonSES.deliver(email, config) ==
+             {:ok, %{id: "messageId", request_id: "requestId"}}
+  end
+
   test "validate_config/1 with valid config", %{config: config} do
     assert ExAwsAmazonSES.validate_config(config) == :ok
   end
