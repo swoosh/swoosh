5f70eb27194bcd80f9b13c9ffb9672e004b91885
Po Chen <chenpaul914@gmail.com>
2024-06-06 09:47:27 +1000

    fix docs generation

--- lib/swoosh.ex
+++ lib/swoosh.ex
@@ -1,6 +1,8 @@
 defmodule Swoosh do
   @external_resource "README.md"
-  @moduledoc File.read!("README.md") |> String.replace("# Swoosh\n\n", "", global: false)
+  @moduledoc File.read!("README.md")
+             |> String.replace("# Swoosh\n\n", "", global: false)
+             |> String.replace("(#adapters", "(#module-adapters")
 
   @version "1.16.9"
 
