f90f6f646110152b3f4b9ee43e4474d11d372cec
Po Chen <chenpaul914@gmail.com>
2025-01-31 12:23:16 +1100

    Compile tailwind instead of using cdn (#998)
    * compile tailwind
    
    * use relative path in case forwarding
    
    * set runtime false
    
    * fix: add csp nonce (#996)
    
    * Update mailbox_preview.ex
    
    * Update mailbox_preview.ex
    
    ---------
    
    Co-authored-by: Yordis Prieto <yordis.prieto@gmail.com>

--- lib/plug/mailbox_preview.ex
+++ lib/plug/mailbox_preview.ex
@@ -3,13 +3,18 @@ if Code.ensure_loaded?(Plug) do
     @moduledoc """
     Plug that serves pages useful for previewing emails in development.
 
+    - `:csp_nonce_assign_key` - a map of keys to assign to the conn.assigns.
+      - `:script` - the key to assign the script CSP nonce to
+      - `:style` - the key to assign the style CSP nonce to
+
     ## Examples
 
         # in a Phoenix router
         defmodule Sample.Router do
           scope "/dev" do
             pipe_through [:browser]
-            forward "/mailbox", Plug.Swoosh.MailboxPreview
+            forward "/mailbox", Plug.Swoosh.MailboxPreview,
+              csp_nonce_assign_key: %{script: :script_csp_nonce, style: :style_csp_nonce}
           end
         end
     """
@@ -30,14 +35,21 @@ if Code.ensure_loaded?(Plug) do
     )
 
     def call(conn, opts) do
+      csp_nonce_assign_key =
+        opts
+        |> Access.get(:csp_nonce_assign_key, [])
+        |> Enum.into(%{script: :script_csp_nonce, style: :style_csp_nonce})
+
       conn =
         conn
         |> assign(:base_path, Path.join(["/" | conn.script_name]))
         |> assign(:storage_driver, opts[:storage_driver] || Memory)
+        |> put_private(:csp_nonce_assign_key, csp_nonce_assign_key)
 
       super(conn, opts)
     end
 
+    plug Plug.Static, at: "/assets", from: "priv/static/assets/"
     plug :match
     plug :dispatch
 
@@ -148,6 +160,11 @@ if Code.ensure_loaded?(Plug) do
 
     defp to_absolute_url(conn, path) do
       Path.join(conn.assigns.base_path, path)
+    end
+
+    defp csp_nonce(conn, type) when type in [:script, :style] do
+      csp_nonce_assign_key = conn.private.csp_nonce_assign_key[type]
+      conn.assigns[csp_nonce_assign_key]
     end
 
     defp render_recipient(recipient) do
