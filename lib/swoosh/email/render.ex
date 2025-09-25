108a5face46cb7056ae2051e7af23865c63dbdce
Jie <jie.gillet@gmail.com>
2025-09-25 14:40:59 +0800

    Encode international domain names (IDNA) (#1070)
    * punyencode domain names
    
    * make dependency explicit and non optional

--- lib/swoosh/email/render.ex
+++ lib/swoosh/email/render.ex
@@ -2,8 +2,8 @@ defmodule Swoosh.Email.Render do
   @moduledoc false
 
   def render_recipient(nil), do: ""
-  def render_recipient({nil, address}), do: address
-  def render_recipient({"", address}), do: address
+  def render_recipient({nil, address}), do: puny_encode(address)
+  def render_recipient({"", address}), do: puny_encode(address)
   def render_recipient([]), do: ""
   def render_recipient("TEMPLATE"), do: "TEMPLATE"
 
@@ -13,11 +13,22 @@ defmodule Swoosh.Email.Render do
       |> String.replace("\\", "\\\\")
       |> String.replace("\"", "\\\"")
 
-    ~s("#{name}" <#{address}>)
+    ~s("#{name}" <#{puny_encode(address)}>)
   end
 
   def render_recipient(list) when is_list(list) do
     list
     |> Enum.map_join(", ", &render_recipient/1)
+  end
+
+  defp puny_encode(email) do
+    case String.split(email, "@") do
+      [local, domain] ->
+        encoded_domain = :idna.utf8_to_ascii(domain)
+        Enum.join([local, encoded_domain], "@")
+
+      _ ->
+        email
+    end
   end
 end
