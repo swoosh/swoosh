a918b71fced8be01f34ae0957853c0657a57d421
Jeremie Gillet <jie.gillet@gmail.com>
2025-09-25 12:58:41 +0900

    make dependency explicit and non optional

--- mix.exs
+++ mix.exs
@@ -70,6 +70,7 @@ defmodule Swoosh.Mixfile do
       {:mime, "~> 1.1 or ~> 2.0"},
       {:jason, "~> 1.0"},
       {:telemetry, "~> 0.4.2 or ~> 1.0"},
+      {:idna, "~> 6.0"},
       {:hackney, "~> 1.9", optional: true},
       {:finch, "~> 0.6", optional: true},
       {:req, "~> 0.5.10 or ~> 0.6 or ~> 1.0", optional: true},
